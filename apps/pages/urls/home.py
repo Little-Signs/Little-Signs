7df3a77dc98131182b37249ed3be62027cce393a
kudakwashe chris chipangura <49289670+kudah99@users.noreply.github.com>
2024-05-27 21:48:48 +0200

    fix #14

--- apps/pages/urls/home.py
+++ apps/pages/urls/home.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='home', permanent=False)),
     path("home/", views.index, name="home"),
-    path("home/coarse/<int:pk>/", views.CoarseDetailView.as_view(), name="coarse-detail"),
+    path("home/study-pack/<int:pk>/", views.CoarseDetailView.as_view(), name="coarse-detail"),
     path("about-us/",views.aboutUs, name="about-us"),
     path("contact/", views.contact, name="contact"),
     path("term-of-use/", views.termOfuse, name="term-of-use"),
